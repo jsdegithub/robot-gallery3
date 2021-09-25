917ba00c3c9e31219d40812cf27f9bb9e44e9a4c
jsdegithub <def_myself@qq.com>
2021-09-25 20:34:47 +0800

    ShoppingCart_class_component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from "./assets/images/logo.svg";
 import styles from "./App.module.css";
 import Robot from "./components/Robot";
 import robots from "./mockdata/robots.json";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <img src={logo} className={styles["appLogo"]} alt="" />
         <h1>罗伯特机器人购物平台</h1>
       </div>
+      <ShoppingCart />
       <div className={styles.robotList}>
         {robots.map((robot) => (
           <Robot id={robot.id} name={robot.name} email={robot.email} />
